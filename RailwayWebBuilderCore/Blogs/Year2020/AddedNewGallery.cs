fb64ac4254015f3805fe9eab11f29f9f96e9386c
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2020-12-04 07:50:45 +0000

    Added blog note about the image image gallary

---
+++ RailwayWebBuilderCore/Blogs/Year2020/AddedNewGallery.cs
@@ -0,0 +1,15 @@
+﻿using RailwayWebBuilderCore.Data;
+using System;
+
+namespace RailwayWebBuilderCore.Blogs.Year2020
+{
+    public class AddedNewGallery : BlogBase
+    {
+        public AddedNewGallery()
+        {
+            Name = "Site update";
+            Date = new DateTime(2020, 12, 05);
+            Paragraph = "Hello, I've finally got around to replacing how the images work, You can now click on one image and then move to the next using the arrows. And I've also increased the size of the images so you can get a better look at the amazing work done by the models.";
+        }
+    }
+}
