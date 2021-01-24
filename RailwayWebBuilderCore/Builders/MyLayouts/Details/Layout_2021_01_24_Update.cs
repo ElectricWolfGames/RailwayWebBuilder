63c542cba3f798e4a5f3a5a34234c93ced675510
Karl Gillott <karlgillott@GoogleMail.com>
2021-01-24 17:28:38 +0000

    002 layout update and name

---
+++ RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_01_24_Update.cs
@@ -0,0 +1,33 @@
+﻿using RailwayWebBuilderCore.Configuration;
+using RailwayWebBuilderCore.Interfaces;
+using System;
+using System.Text;
+
+namespace RailwayWebBuilderCore.Builders.MyLayouts.Details
+{
+    public class Layout_2021_01_24_Update : ILayoutPagesDetails
+    {
+        public string RawImagePath { get; }
+        public string ExportImagePath { get; }
+        public string YouTubeLink { get; }
+        public string Title { get; }
+        public StringBuilder Details { get; }
+        public DateTime When { get; }
+
+        public Layout_2021_01_24_Update()
+        {
+            When = new DateTime(2021, 01, 24);
+            Details = new StringBuilder();
+
+            RawImagePath = Constants.RawDataPath + @"MyLayout\2021-01-24 Update\";
+            ExportImagePath = Constants.FullMyLayouts + "images\\010\\";
+
+            Title = "<h2>Name and update</h2>";
+
+            Details.Append("<p>We have named our layout - watch to find out...</p>");
+            Details.Append("<p>And some other update around the boards before we can do anything else.</p>");
+
+            YouTubeLink = "NvHYQiO52n8";
+        }
+    }
+}
