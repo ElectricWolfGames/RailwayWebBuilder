9dbb7c747bff623d67837ef68f39f23a0aa8840f
Karl Gillott <karlgillott@GoogleMail.com>
2021-02-11 16:41:16 +0000

    Added place holder for next upload

---
+++ RailwayWebBuilderCore/Blogs/Year2021/Blog_2021_02_14_Layout4Weeks.cs
@@ -0,0 +1,17 @@
+﻿using RailwayWebBuilderCore.Data;
+using System;
+
+namespace RailwayWebBuilderCore.Blogs.Year2021
+{
+    public class Blog_2021_02_14_Layout4Weeks : BlogBase
+    {
+        public Blog_2021_02_14_Layout4Weeks()
+        {
+            Name = "4 weeks in and we are still building";
+            Date = new DateTime(2021, 02, 14);
+            Paragraph = "We have made a lot of progress, but still much to do.";
+            Link = "MyLayouts/index.html";
+            KeyImage = @"MyLayouts\images\012\P1161510.JPG";
+        }
+    }
+}
---
+++ RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_02_14_StuckRails_Church.cs
@@ -0,0 +1,35 @@
+﻿using RailwayWebBuilderCore.Configuration;
+using RailwayWebBuilderCore.Interfaces;
+using System;
+using System.Text;
+
+namespace RailwayWebBuilderCore.Builders.MyLayouts.Details
+{
+    public class Layout_2021_02_14_StuckRails_Church : ILayoutPagesDetails
+    {
+        public string RawImagePath { get; }
+        public string ExportImagePath { get; }
+        public string YouTubeLink { get; }
+        public string Title { get; }
+        public StringBuilder Details { get; }
+        public DateTime When { get; }
+
+        public Layout_2021_02_14_StuckRails_Church()
+        {
+            When = new DateTime(2021, 02, 14);
+            Details = new StringBuilder();
+
+            RawImagePath = Constants.RawDataPath + @"MyLayout\2021-02-14 StuckRails Church\";
+            ExportImagePath = Constants.FullMyLayouts + "images\\0214\\";
+
+            Title = "<h2>Stuck done some track and updated the Church</h2>";
+
+            Details.Append("<p>We have started to glue the track down, currently I don't have enought cork to finish the work.</p>");
+            Details.Append("<p>But at least I've been able to get one track on to the swing out section.</p>");
+
+            Details.Append("<p>And we have added the scenery around the church</p>");
+
+            YouTubeLink = "????????";
+        }
+    }
+}
