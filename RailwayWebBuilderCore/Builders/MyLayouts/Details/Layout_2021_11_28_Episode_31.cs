bcd61ad1c04e15b8baa4498baa45ae6edaba7a8e
Karl Gillott <karlgillott@GoogleMail.com>
2021-11-28 06:47:26 +0000

    Added Tornado and Ep 31

---
+++ RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_11_28_Episode_31.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using RailwayWebBuilderCore.Configuration;
+
+namespace RailwayWebBuilderCore.Builders.MyLayouts.Details
+{
+    public class Layout_2021_11_28_Episode_31 : LayoutBase
+    {
+        public Layout_2021_11_28_Episode_31()
+        {
+            Active = true;
+            When = new DateTime(2021, 11, 28);
+            Details = new StringBuilder();
+
+            RawImagePath = Constants.RawDataPath + @"MyLayout\2021-11-28 31\";
+            ExportImagePath = Constants.FullMyLayouts + "images\\1128\\";
+
+            Title = "<h2>31: Locos at Cattington</h2>";
+            Details.Append("<p>Welcome back to Cattington,</p>");
+
+            Details.Append("<p>Lets take a look at all the loco we have.</p>");
+
+            YouTubeLink = "TlRAAubVj2o";
+        }
+    }
+}
