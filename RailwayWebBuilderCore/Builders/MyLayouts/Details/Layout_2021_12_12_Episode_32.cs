1b81cda72743f4c76868a7fd5f82e3a54fddaa89
Karl Gillott <karlgillott@GoogleMail.com>
2021-12-08 07:33:40 +0000

    Added new Episodes

--- RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_12_12_Episode_32.cs
+++ RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_12_12_Episode_32.cs
@@ -4,23 +4,23 @@ using RailwayWebBuilderCore.Configuration;
 
 namespace RailwayWebBuilderCore.Builders.MyLayouts.Details
 {
-    public class Layout_2021_11_28_Episode_31 : LayoutBase
+    public class Layout_2021_12_12_Episode_32 : LayoutBase
     {
-        public Layout_2021_11_28_Episode_31()
+        public Layout_2021_12_12_Episode_32()
         {
-            Active = true;
-            When = new DateTime(2021, 11, 28);
+            Active = false;
+            When = new DateTime(2021, 12, 12);
             Details = new StringBuilder();
 
-            RawImagePath = Constants.RawDataPath + @"MyLayout\2021-11-28 31\";
-            ExportImagePath = Constants.FullMyLayouts + "images\\1128\\";
+            RawImagePath = Constants.RawDataPath + @"MyLayout\2021-12-12 32\";
+            ExportImagePath = Constants.FullMyLayouts + "images\\1212\\";
 
             Title = "<h2>31: Locos at Cattington</h2>";
             Details.Append("<p>Welcome back to Cattington,</p>");
 
             Details.Append("<p>Lets take a look at all the loco we have.</p>");
 
-            YouTubeLink = "TlRAAubVj2o";
+            YouTubeLink = "";
         }
     }
 }
--- RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_12_26_Episode_33.cs
+++ RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_12_26_Episode_33.cs
@@ -4,23 +4,23 @@ using RailwayWebBuilderCore.Configuration;
 
 namespace RailwayWebBuilderCore.Builders.MyLayouts.Details
 {
-    public class Layout_2021_11_28_Episode_31 : LayoutBase
+    public class Layout_2021_12_26_Episode_33 : LayoutBase
     {
-        public Layout_2021_11_28_Episode_31()
+        public Layout_2021_12_26_Episode_33()
         {
-            Active = true;
-            When = new DateTime(2021, 11, 28);
+            Active = false;
+            When = new DateTime(2021, 12, 26);
             Details = new StringBuilder();
 
-            RawImagePath = Constants.RawDataPath + @"MyLayout\2021-11-28 31\";
-            ExportImagePath = Constants.FullMyLayouts + "images\\1128\\";
+            RawImagePath = Constants.RawDataPath + @"MyLayout\2021-12-26 33\";
+            ExportImagePath = Constants.FullMyLayouts + "images\\1226\\";
 
             Title = "<h2>31: Locos at Cattington</h2>";
             Details.Append("<p>Welcome back to Cattington,</p>");
 
             Details.Append("<p>Lets take a look at all the loco we have.</p>");
 
-            YouTubeLink = "TlRAAubVj2o";
+            YouTubeLink = "";
         }
     }
 }
