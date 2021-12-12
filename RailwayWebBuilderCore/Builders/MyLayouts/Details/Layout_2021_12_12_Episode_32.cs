8a4e322077a96843508780d3d3a5702dea384f9e
Karl Gillott <karlgillott@GoogleMail.com>
2021-12-12 07:46:40 +0000

    Update Details for Episode 32

--- RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_12_12_Episode_32.cs
+++ RailwayWebBuilderCore/Builders/MyLayouts/Details/Layout_2021_12_12_Episode_32.cs
@@ -8,19 +8,19 @@ namespace RailwayWebBuilderCore.Builders.MyLayouts.Details
     {
         public Layout_2021_12_12_Episode_32()
         {
-            Active = false;
+            Active = true;
             When = new DateTime(2021, 12, 12);
             Details = new StringBuilder();
 
             RawImagePath = Constants.RawDataPath + @"MyLayout\2021-12-12 32\";
             ExportImagePath = Constants.FullMyLayouts + "images\\1212\\";
 
-            Title = "<h2>31: Locos at Cattington</h2>";
+            Title = "<h2>32: The Castle is Done!</h2>";
             Details.Append("<p>Welcome back to Cattington,</p>");
 
-            Details.Append("<p>Lets take a look at all the loco we have.</p>");
+            Details.Append("<p>We have finished the Castle Tyson on the Hill.</p>");
 
-            YouTubeLink = "";
+            YouTubeLink = "W7zxbgAwq5s";
         }
     }
 }
