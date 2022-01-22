f14dbed3ca00f6ee9040398c6a55c83f72b93ac3
Karl Gillott <karlgillott@GoogleMail.com>
2022-01-22 08:11:03 +0000

    Added Blog info for room tour

---
+++ RailwayWebBuilderCore/Blogs/Year2022/Blog_2022_01_23_RoomTour.cs
@@ -0,0 +1,17 @@
+﻿using RailwayWebBuilderCore.Data;
+using System;
+
+namespace RailwayWebBuilderCore.Blogs.Year2021
+{
+    public class Blog_2022_01_23_RoomTour : BlogBase
+    {
+        public Blog_2022_01_23_RoomTour()
+        {
+            Name = "Full Room Tour";
+            Date = new DateTime(2022, 01, 23);
+            Paragraph = "As Cattington is now one year old, we are going to do a full room tour.";
+            Link = "MyLayouts/index.html";
+            KeyImage = @"MyLayouts\images\220123\P1156590.JPG";
+        }
+    }
+}
