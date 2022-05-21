65c4d6fad25ab5bccc2182d62a0e0f584f253daa
Karl Gillott <KarlGillott@googleMail.com>
2022-05-21 08:56:56 +0100

    added LutterworthSportsCentre

---
+++ RailwayWebBuilderCore/ModelRailways/EventLocations/LutterworthSportsCentre.cs
@@ -0,0 +1,15 @@
+﻿using RailwayWebBuilderCore.Data;
+
+namespace RailwayWebBuilderCore.ModelRailways.EventLocations
+{
+    public class LutterworthSportsCentre : EventLocationsBase
+    {
+        public LutterworthSportsCentre()
+        {
+            Name = "Lutterworth Sports Centre";
+            LatitudeLongitude = new LatitudeLongitude(52.4580745, -1.2064586);
+            Address = "Lutterworth Sports Centre, Coventry Road, Lutterworth, Leicestershire LE17 4RB";
+            PostCode = "LE17 4RB";
+        }
+    }
+}
