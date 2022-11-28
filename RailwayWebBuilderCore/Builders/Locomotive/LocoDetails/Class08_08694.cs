517d0cc2f9c1a48fb586f0cf725f3fe65b7b5808
Karl Gillott <KarlGillott@googleMail.com>
2022-11-28 07:19:06 +0000

    Added find tags

--- RailwayWebBuilderCore/Builders/Locomotive/LocoDetails/Class08_08694.cs
+++ RailwayWebBuilderCore/Builders/Locomotive/LocoDetails/Class08_08694.cs
@@ -12,6 +12,8 @@ namespace RailwayWebBuilderCore.Builders.Locomotive.LocoDetails
             HtmlFileName = $"{PageTitle}.html";
             RawImagePath = Constants.RawDataPath + @"\Locos\Class08-08694\";
             ExportImagePath = Constants.LocoRef + "images\\08694\\";
+
+            FindTags = "08694";
         }
     }
 }
