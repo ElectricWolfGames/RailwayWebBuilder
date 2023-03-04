4de45798168d5dbef49b0eacf815785da0fc8833
Karl Gillott <KarlGillott@googleMail.com>
2023-03-04 07:25:29 +0000

    Update donation page

--- RailwayWebBuilderCore/Builders/GCR/GCRTextToDonate.cs
+++ RailwayWebBuilderCore/Builders/GCR/GCRTextToDonate.cs
@@ -5,6 +5,7 @@ using RailwayWebBuilderCore.Headers;
 using RailwayWebBuilderCore.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -29,36 +30,185 @@ namespace RailwayWebBuilderCore.Builders.GCR
 
             List<DonateAmount> donateAmounts = LoadDonations();
 
-            var d29 = donateAmounts.Where(x => x.When.Contains("27 Jan 2023")).ToList();
-
             _pageBuilder = new PageBuilder("index.html", LocalPath, CreateHeader(), "../");
 
             _pageBuilder.Append(NavBarHelper.NavBar("../"));
             _pageBuilder.Append("<div class='container mt-4'>");
             _pageBuilder.Append(Jumbotron());
-            _pageBuilder.Append("<div class='row mb-2'>");
 
-            //_pageBuilder.Append("<div class='container mt-4'>");
-            //_pageBuilder.Append("</br>");
-
-            _pageBuilder.Append(AddGraph(donateAmounts));
+            _pageBuilder.Append(AddGraphLastTenDays(donateAmounts));
+            _pageBuilder.Append(AddGraphByMonth(donateAmounts));
 
             _pageBuilder.Append("</div>");
+            _pageBuilder.Append("</br>");
+            _pageBuilder.Append("</br>");
 
             _pageBuilder.Output();
         }
 
         protected IPageHeader CreateHeader()
         {
-            var header = new ShopGCRAuctionsHeader();
+            var header = new GCRTextToDonateHeader();
             header.Title = "Donate";
             return header;
         }
 
-        private string AddGraph(List<DonateAmount> donateAmounts)
+        private string AddGraph(string name, string chartName, string dates, string greenValue, string redValues)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("var xValues = [100,200,300,400,500,600,700,800,900,1000];\r\n\r\nnew Chart('myChart', {\r\n  type: 'line',\r\n  data: {\r\n    labels: xValues,\r\n    datasets: [{\r\n      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],\r\n      borderColor: 'red',\r\n      fill: false\r\n    },{\r\n      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],\r\n      borderColor: 'green',\r\n      fill: false\r\n    },{\r\n      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],\r\n      borderColor: 'blue',\r\n      fill: false\r\n    }]\r\n  },\r\n  options: {\r\n    legend: {display: false}\r\n  }\r\n});");
+            sb.AppendLine("<div class='row'>");
+            sb.AppendLine("<div class='col-md-8'>");
+
+            sb.AppendLine($"<h3>{name}</h3>");
+            sb.Append($" <canvas id='{chartName}'></canvas>");
+
+            sb.Append("</div>");
+            sb.Append("</div>");
+
+            sb.Append("<script>" +
+                $"var xValues = [{dates}];" +
+                $"new Chart('{chartName}', {{" +
+                "  type: 'line'," +
+                "  data: {" +
+                "    labels: xValues," +
+                "    datasets: [{" +
+                $"      data: [{greenValue}]," +
+                "      borderColor: 'green'," +
+                "      fill: false," +
+                "      label: 'Donation'" +
+                "    },{" +
+                $"      data: [{redValues}]," +
+                "      borderColor: 'red'," +
+                "      fill: false," +
+                "      label: 'Total'" +
+                "    }]" +
+                "  }," +
+                "  options: {" +
+                "    legend: {display: true}" +
+                "  }" +
+                "});</script>");
+
+            return sb.ToString();
+        }
+
+        private string AddGraphByMonth(List<DonateAmount> donateAmounts)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            donateAmounts = donateAmounts.OrderByDescending(x => x.When).ToList();
+            List<DonateAmount> dayTotals = new List<DonateAmount>();
+
+            DateTime currentDate = DateTime.Now;
+            for (int i = 0; i < 25; i++)
+            {
+                DonateAmount donateAmount = new DonateAmount();
+                donateAmount.When = currentDate;
+                donateAmount.Total = 0;
+                donateAmount.GiftAid = 0;
+                donateAmount.Donation = 0;
+                dayTotals.Add(donateAmount);
+                currentDate = currentDate.AddMonths(-1);
+            }
+
+            int currIndex = 0;
+            int count = 0;
+            while (count < 350)
+            {
+                var donate = donateAmounts[currIndex++];
+
+                var added = dayTotals.FirstOrDefault(x => x.When.Year == donate.When.Year
+                                    && x.When.Month == donate.When.Month);
+
+                if (added == null)
+                    break;
+
+                count++;
+                added.Total += donate.Total;
+                added.Donation += donate.Donation;
+                added.GiftAid += donate.GiftAid;
+            }
+
+            List<DateTime> current = new List<DateTime>();
+
+            List<decimal> dayColumsDonateTotals = new List<decimal>();
+            List<decimal> dayColumnTotals = new List<decimal>();
+            string dates = "";
+
+            for (int i = 15; i > 0; i--)
+            {
+                dayColumsDonateTotals.Add(dayTotals[i].Donation);
+                dayColumnTotals.Add(dayTotals[i].Total);
+                if (dates.Length != 0)
+                    dates += ",";
+                dates += $"\"{dayTotals[i].When.Year}/{dayTotals[i].When.Month.ToString("00")}\"";
+            }
+            string dayDonateTotalList = string.Join(",", dayColumsDonateTotals);
+
+            string dayTotalList = string.Join(",", dayColumnTotals);
+
+            sb.Append(AddGraph("By month", "Month", dates, dayDonateTotalList, dayTotalList));
+
+            return sb.ToString();
+        }
+
+        private string AddGraphLastTenDays(List<DonateAmount> donateAmounts)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            donateAmounts = donateAmounts.OrderByDescending(x => x.When).ToList();
+
+            List<DonateAmount> dayTotals = new List<DonateAmount>();
+            int currIndex = 0;
+            int count = 0;
+
+            DateTime currentDate = DateTime.Now;
+            for (int i = 0; i < 100; i++)
+            {
+                DonateAmount donateAmount = new DonateAmount();
+                donateAmount.When = currentDate;
+                donateAmount.Total = 0;
+                donateAmount.GiftAid = 0;
+                donateAmount.Donation = 0;
+                dayTotals.Add(donateAmount);
+                currentDate = currentDate.AddDays(-1);
+            }
+
+            while (count < 100)
+            {
+                var donate = donateAmounts[currIndex++];
+
+                var added = dayTotals.FirstOrDefault(x => x.When.Year == donate.When.Year
+                                    && x.When.Month == donate.When.Month
+                                    && x.When.Day == donate.When.Day);
+
+                count++;
+                if (added != null)
+                {
+                    added.Total += donate.Total;
+                    added.Donation += donate.Donation;
+                    added.GiftAid += donate.GiftAid;
+                }
+            }
+
+            List<DateTime> current = new List<DateTime>();
+
+            List<decimal> dayColumsDonateTotals = new List<decimal>();
+            List<decimal> dayColumnTotals = new List<decimal>();
+            string dates = "";
+
+            for (int i = 20; i > 0; i--)
+            {
+                dayColumsDonateTotals.Add(dayTotals[i].Donation);
+                dayColumnTotals.Add(dayTotals[i].Total);
+                if (dates.Length != 0)
+                    dates += ",";
+                dates += $"\"{dayTotals[i].When.Year}/{dayTotals[i].When.Month.ToString("00")}/{dayTotals[i].When.Day}\"";
+            }
+            string dayDonateTotalList = string.Join(",", dayColumsDonateTotals);
+
+            string dayTotalList = string.Join(",", dayColumnTotals);
+
+            sb.Append(AddGraph("Last 20 Days", "Days", dates, dayDonateTotalList, dayTotalList));
 
             return sb.ToString();
         }
@@ -96,24 +246,38 @@ namespace RailwayWebBuilderCore.Builders.GCR
         {
             var donations = new List<DonateAmount>();
 
-            string path = @"C:\Users\Oem\Downloads\31-01-23-donations.csv";
+            string path = @"C:\Users\Oem\Downloads\04-03-23-donations.csv";
 
             string doc = File.ReadAllText(path);
             string[] lines = doc.Split("\n");
 
             foreach (var line in lines)
             {
-                string[] parts = line.Split(",");
+                var lineClean = line.Replace("\"", "");
+
+                string[] parts = lineClean.Split(",");
                 if (parts.Length > 10)
                 {
                     if (parts[0].Contains("name"))
                         continue;
 
+                    DateTime temp;
+                    DateTime.TryParse(parts[6], out temp);
+
                     DonateAmount donateAmount = new DonateAmount();
-                    donateAmount.Donation = parts[3];
-                    donateAmount.GiftAid = parts[4];
-                    donateAmount.Total = parts[5];
-                    donateAmount.When = parts[6];
+                    if (!string.IsNullOrWhiteSpace(parts[3].Trim()))
+                    {
+                        donateAmount.Donation = decimal.Parse(parts[3]);
+                    }
+                    if (!string.IsNullOrWhiteSpace(parts[4].Trim()))
+                    {
+                        donateAmount.GiftAid = decimal.Parse(parts[4]);
+                    }
+                    if (!string.IsNullOrWhiteSpace(parts[5].Trim()))
+                    {
+                        donateAmount.Total = decimal.Parse(parts[5]);
+                    }
+                    donateAmount.When = temp;
                     donateAmount.Method = parts[10];
                     donations.Add(donateAmount);
                 }
@@ -124,11 +288,11 @@ namespace RailwayWebBuilderCore.Builders.GCR
 
         private class DonateAmount
         {
-            public string Donation { get; set; }
-            public string GiftAid { get; set; }
+            public decimal Donation { get; set; }
+            public decimal GiftAid { get; set; }
             public string Method { get; set; }
-            public string Total { get; set; }
-            public string When { get; set; }
+            public decimal Total { get; set; }
+            public DateTime When { get; set; }
         }
     }
 }
