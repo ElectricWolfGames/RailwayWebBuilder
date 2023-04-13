f42c88420744e947b0278cd748693e399179781f
Karl Gillott <KarlGillott@googleMail.com>
2023-04-13 18:42:19 +0100

    Updte Wagons

---
+++ RailwayWebBuilderCore/Builders/Locomotive/LocoDetails/Wagons/Tanker_VTG60874.cs
@@ -0,0 +1,24 @@
+﻿using RailwayWebBuilderCore.Configuration;
+
+namespace RailwayWebBuilderCore.Builders.Locomotive.LocoDetails.Wagons
+{
+    public class Tanker_VTG60874 : LocomotiveDetailsBase
+    {
+        public Tanker_VTG60874()
+        {
+            Title = "Tanker VTG60874";
+            PageTitle = "VTG60874";
+            HtmlFileName = $"{PageTitle}.html";
+            RawImagePath = Constants.RawDataPath + @"\Locos\_Wagons\VTG60874\";
+            ExportImagePath = Constants.LocoRef + "images\\VTG60874\\";
+            Paragraph1 = "";
+            Built = "";
+            Order = 1;
+            Wheelbase = "";
+            StockType = StockTypes.Wagon;
+
+            FindTags = "VTG60874";
+            Keywords.Add("Tanker");
+        }
+    }
+}
