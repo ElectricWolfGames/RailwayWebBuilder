ac5ca34e0d107a8c80d1a54ab537d12f1b48b884
Karl Gillott <karlgillott@googlemail.com>
2023-06-13 21:12:46 +0100

    Added 54

---
+++ RailwayWebBuilderCore/Blogs/Year2023/Blog_2023_06_18_Cattington54.cs
@@ -0,0 +1,17 @@
+﻿using RailwayWebBuilderCore.Data;
+using System;
+
+namespace RailwayWebBuilderCore.Blogs.Year2023
+{
+    public class Blog_2023_06_18_Cattington54 : BlogBase
+    {
+        public Blog_2023_06_18_Cattington54()
+        {
+            Name = "Cattington Update 54";
+            Date = new DateTime(2023, 06, 18);
+            Paragraph = "Funfair and a Guest Loco";
+            Link = "MyLayouts/index.html";
+            KeyImage = @"MyLayouts\images\230618\IMG_3990-A5-thumb.JPG";
+        }
+    }
+}
