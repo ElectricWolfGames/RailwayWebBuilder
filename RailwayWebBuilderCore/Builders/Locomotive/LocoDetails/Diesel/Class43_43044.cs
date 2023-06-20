3999b540b20db6ab21a3c8bc6ab6353e63a6b9db
Karl Gillott <karlgillott@googlemail.com>
2023-06-20 07:42:07 +0100

    Added more locos

--- RailwayWebBuilderCore/Builders/Locomotive/LocoDetails/Diesel/Class43_43044.cs
+++ RailwayWebBuilderCore/Builders/Locomotive/LocoDetails/Diesel/Class43_43044.cs
@@ -1,47 +1,23 @@
-﻿using RailwayWebBuilderCore.Configuration;
+﻿using RailwayWebBuilderCore.Builders.Locomotive.LocoDetails.Class;
+using RailwayWebBuilderCore.Configuration;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace RailwayWebBuilderCore.Builders.Locomotive.LocoDetails.Diesel
 {
-    public class Class43_43044 : LocomotiveDetailsBase
+    public class Class43_43044: Class43
     {
-        public Class43_43044()
+        public Class43_43044() : base()
         {
             Title = "43044, Class 43";
-            Class = "Class 43";
             PageTitle = "43044";
             HtmlFileName = $"{PageTitle}.html";
             RawImagePath = Constants.RawDataPath + @"\Locos\Class43-43044\";
             ExportImagePath = Constants.LocoRef + "images\\43044\\";
-            Order = 2;
-            StockType = StockTypes.Diesel;
+            FindTags = "43044,Class43";
 
-            Paragraph1 = "";// TODO Add loco notes
-            Paragraph2 = "";
             Paragraph3 = "";
 
             Keywords.Add("HST");
-        }
-    }
-
-    public class Ruston_319290 : LocomotiveDetailsBase
-    {
-        public Ruston_319290()
-        {
-            Title = "319290, Ruston & Hornsby, 0-4-0";
-            Class = "Ruston & Hornsby";
-            PageTitle = "319290";
-            HtmlFileName = $"{PageTitle}.html";
-            RawImagePath = Constants.RawDataPath + @"\Locos\Ruston_319290\";
-            ExportImagePath = Constants.LocoRef + "images\\319290\\";
-            Order = 2;
-            StockType = StockTypes.Diesel;
-            FindTags = "319290";
-
-            Paragraph1 = "";// TODO Add loco notes
-            Paragraph2 = "";
-            Paragraph3 = "";
-
-            Keywords.Add(Title);
         }
     }
 }
