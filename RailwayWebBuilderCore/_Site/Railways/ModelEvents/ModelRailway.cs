9deca215ab3f3fed3c1d1ef1c30f34a3c966a51d
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2023-09-19 07:40:08 +0100

    Adding Model Events

---
+++ RailwayWebBuilderCore/_Site/Railways/ModelEvents/ModelRailway.cs
@@ -0,0 +1,103 @@
+﻿using eWolfBootstrap.SiteBuilder;
+using eWolfBootstrap.SiteBuilder.Attributes;
+using eWolfBootstrap.SiteBuilder.Enums;
+using RailwayWebBuilderCore._Site.Railways.Locomotives;
+using RailwayWebBuilderCore._Site.Railways.MyLayouts;
+using RailwayWebBuilderCore.Builders.Locomotive.LocoDetails;
+using RailwayWebBuilderCore.Builders.ModelEvents;
+using RailwayWebBuilderCore.Configuration;
+using RailwayWebBuilderCore.Headers;
+using RailwayWebBuilderCore.Interfaces;
+using RailwayWebBuilderCore.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace RailwayWebBuilderCore._Site.Railways.ModelEvents
+{
+    [PageTitle("index.html")]
+    [Navigation(NavigationTypes.Main, 1)]
+    internal class ModelRailway : PageDetails
+    {
+        public ModelRailway()
+        {
+            WebPage = new WebPage(this);
+            DisplayTitle = "Model Events";
+            MenuTitle = "Model Events";
+        }
+
+        public override void CreatePage()
+        {
+            ModelEventDetailsServices meds = ServiceLocator.Instance.GetService<ModelEventDetailsServices>();
+
+            var meh = new ModelEventsHeader();
+
+            foreach (IModelEvent modelEvent in meds.Events)
+            {
+                meh.Keywords.AddRange(modelEvent.Layouts.Select(x => x.Name));
+            }
+
+            string htmlpath = Constants._aRootPath + "//" + Constants.ModelEvents + "//";
+
+
+            WebPage.AddHeader(this);
+            WebPage.AddNavigation(NavigationTypes.Main, @"../../");
+            WebPage.StartBody();
+
+
+            WebPage.Append("<div class='container mt-12'>");
+
+            WebPage.Append(LocoRef.CreateHero(this));
+
+
+            var ordedBlogs = meds.Events.OrderByDescending(x => x.TripDate);
+            WebPage.Append("<div class='row mb-2'>");
+
+            foreach (IModelEvent modelEvent in ordedBlogs)
+            {
+                WebPage.Append(CreateBlog(modelEvent));
+
+                CreatModelLayoutPage(modelEvent);
+            }
+
+            WebPage.Append("</div>");
+            WebPage.Append("</div>");
+
+            WebPage.EndBody();
+            WebPage.Output();
+        }
+
+        private static string CreateBlog(IModelEvent blog)
+        {
+            StringBuilder blogHtml = new StringBuilder();
+
+            blogHtml.AppendLine("<div class='col-md-6'>");
+            blogHtml.AppendLine("<div class='card border-dark mb-3'>");
+            blogHtml.AppendLine($"<h5 class='card-header'>{blog.Title}</h5>");
+            blogHtml.AppendLine("<div class='card-body'>");
+            blogHtml.AppendLine($"<h6>{blog.TripDate.ToShortDateString()}</h6>");
+            blogHtml.AppendLine($"      <a href='{blog.ImageFolder}/index.html'><img class='rounded float-right' width='214px' height ='160px'src='{blog.ImageFolder}\\images\\{blog.ImagePreview}'></a>");
+            blogHtml.AppendLine($"<p class='col-md-6 card-text float-left'>{blog.Descrption}</p>");
+            blogHtml.AppendLine($"<p class='col-md-6 '><a href='{blog.ImageFolder}/index.html' class='font-weight-bold'>See more</a></p>");
+            blogHtml.AppendLine("</div>");
+            blogHtml.AppendLine("</div>");
+            blogHtml.AppendLine("</div>");
+
+            return blogHtml.ToString();
+        }
+
+        private void CreatModelLayoutPage(IModelEvent modelEvent)
+        {
+            ModelRailwayPageDetails cattingtonPageDetails = new ModelRailwayPageDetails();
+            cattingtonPageDetails.ModelEvent = modelEvent;
+
+            cattingtonPageDetails.MenuTitle = "index";
+
+            // need to sort out folder
+            cattingtonPageDetails.CreatePage();
+        }
+    }
+}
---
+++ RailwayWebBuilderCore/_Site/Railways/ModelEvents/ModelRailwayPageDetails.cs
@@ -0,0 +1,114 @@
+﻿using eWolfBootstrap.Builders;
+using eWolfBootstrap.SiteBuilder;
+using eWolfBootstrap.SiteBuilder.Attributes;
+using eWolfBootstrap.SiteBuilder.Enums;
+using RailwayWebBuilderCore.Configuration;
+using RailwayWebBuilderCore.Helpers;
+using RailwayWebBuilderCore.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RailwayWebBuilderCore._Site.Railways.ModelEvents
+{
+    [PageTitle("Place holder Page")]
+    [Navigation(NavigationTypes.Main, 2)]
+    public class ModelRailwayPageDetails : PageDetails
+    {
+        public IModelEvent ModelEvent;
+
+        public ModelRailwayPageDetails()
+        {
+            WebPage = new WebPage(this);
+            DisplayTitle = "bah";
+            MenuTitle = "hab";
+            DontShowNavigation = true;
+            DontBuildPage = true;
+        }
+
+        public override void CreatePage()
+        {
+            WebPage.AddHeader(this);
+            WebPage.AddNavigation(NavigationTypes.Main, @"../../../");
+            WebPage.StartBody();
+
+            WebPage.Append("<div class='container mt-12'>");
+
+            WebPage.Append(CreateHero());
+            WebPage.HtmlPath = Constants.MyLayouts;
+            WebPage.HtmlTitle = $"index.html";
+
+            WebPage.SetRootAddress = RootAddress = @"E:\eWolfSiteUploads\Railways"; // TODO Make this a const!
+            WebPage.SetDontBuild = false;
+
+          
+            
+            // Show event details 
+
+            WebPage.Append("</div>");
+            WebPage.Append(HTMLRailHelper.Modal());
+            WebPage.Append("<script src='../Scripts/script.js'></script>");
+
+            //WebPage.EndBody();
+            //WebPage.Output();
+        }
+
+        private static string AddYoutubePreview(string youTubeLink)
+        {
+            var pageBuilder = new HTMLBuilder();
+
+            pageBuilder.Text("<div class='col-md-8'>");
+            pageBuilder.Text("<div class='embed-responsive embed-responsive-16by9'>");
+            pageBuilder.Text($"<iframe src='{youTubeLink}' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+            pageBuilder.Text("</div>");
+            pageBuilder.Text("</div>");
+
+            return pageBuilder.Output();
+        }
+
+        private string BuildDetails(ILayoutPagesDetails detail)
+        {
+            HTMLBuilder pageBuilder = new HTMLBuilder();
+
+            if (!detail.Active)
+                return "";
+
+            pageBuilder.Text($"<hr/>");
+            pageBuilder.Text(detail.Title);
+            pageBuilder.Text(detail.When.ToShortDateString());
+            pageBuilder.Text(detail.Details.ToString());
+
+            if (!string.IsNullOrWhiteSpace(detail.YouTubeLink))
+            {
+                string youTubeLink = $"https://www.youtube.com/embed/{detail.YouTubeLink}";
+                pageBuilder.Text(AddYoutubePreview(youTubeLink));
+            }
+
+            if (!string.IsNullOrWhiteSpace(detail.ExportImagePath))
+            {
+                Directory.CreateDirectory(detail.ExportImagePath);
+                var pb = new PageBuilder();
+                pb.AddImages(Constants.FullMyLayouts, detail.ExportImagePath, detail.RawImagePath);
+                pageBuilder.Text(pb.GetOutput());
+            }
+
+            return pageBuilder.Output();
+        }
+
+        private string CreateHero()
+        {
+            string imageName = Constants._aaDriveLetter + "Trains/_WebsiteData/Others/images/";
+            string imageHtmlName = "../Others/images/";
+
+            HTMLBuilder pageBuilder = new HTMLBuilder();
+            pageBuilder.JumbotronWithImage("<h1>Cattington</h1>",
+                "<p'>Now it's our turn, We have just started to build our first layout." +
+                "</br>We are Building Cattington, and we are updatd details here every week or so.</p>",
+                imageName,
+                imageHtmlName,
+                "CATTINGTON.png");
+
+            return pageBuilder.Output();
+        }
+    }
+}
