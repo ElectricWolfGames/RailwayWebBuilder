97ed7ffd5518272be1218dddd7cb7c5056c61211
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2023-09-27 16:50:37 +0100

    Place holder for cattington 59

---
+++ RailwayWebBuilderCore/_SiteData/MyLayouts/Details/Layout_2023_10_01_Episode_59.cs
@@ -0,0 +1,51 @@
+﻿using RailwayWebBuilderCore.Data;
+using RailwayWebBuilderCore.Interfaces;
+using System;
+
+namespace RailwayWebBuilderCore._SiteData.MyLayouts.Details
+{
+    /*
+    public class Layout_2023_10_01_Episode_59 : LayoutBase, IBlogger
+    {
+        public Layout_2023_10_01_Episode_59()
+        {
+            Active = true;
+            When = new DateTime(2023, 10, 01);
+            EpisodeNumber = 59;
+
+            InitDatedPaths();
+
+            Title = "<h2>59: ?</h2>";
+            Details.Append("<p>Hello and welcome back to Karl and Debbie trains, And we have another Cattington model railway layout update for you.</p>");
+            Details.Append("</br>");
+            Details.Append("<p>This time we have a new tool to show you - a track tester. Some new ocean wagons and some guest loco's</p>");
+            Details.Append("<p>The Track tester is a 'Train tech Multi gauge track tester' we picked up for £10. And the guest locos are all 28's</p>");
+
+            Details.Append("</br>");
+            Details.Append("<ul>");
+            Details.Append("<li>2233 Hornby Dublo Co-Bo Diesel Electric Class 28 D5702</li>");
+            Details.Append("<li>28031 Heljan Class 28 D5717 Br green with small yellow ends</li>");
+            Details.Append("<li>Hattons Heljan Metrovic CL28 Co-Bo D5709 Br Green Weathered</li>");
+            Details.Append("</ul>");
+            Details.Append("</br>");
+            Details.Append("<p>In the Running session, we start with Class 07 D2988 and Class 08 08648 building up a freight train for Class 28.</p>");
+            Details.Append("</br>");
+            Details.Append("</br>");
+            Details.Append("<p>Thank you for watching, Take care</p>");
+            YouTubeLink = "";
+        }
+
+        public IBlog GetBlog()
+        {
+            return new Blog
+            {
+                Name = $"Cattington Update {EpisodeNumber}",
+                Date = When,
+                Link = "MyLayouts/index.html",
+                Paragraph = "Something",
+                KeyImage = $@"MyLayouts\images\{When.ToString("yyyyMMdd")}\IMG_4595-thumb.JPG",
+            };
+        }
+    }
+    */
+}
