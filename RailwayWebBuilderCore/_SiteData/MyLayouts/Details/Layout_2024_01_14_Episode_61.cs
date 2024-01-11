b02fa5fced6a6321ed7e735af9393f164de65b80
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2024-01-11 07:28:42 +0000

    Update 61

--- RailwayWebBuilderCore/_SiteData/MyLayouts/Details/Layout_2024_01_14_Episode_61.cs
+++ RailwayWebBuilderCore/_SiteData/MyLayouts/Details/Layout_2024_01_14_Episode_61.cs
@@ -34,7 +34,7 @@ namespace RailwayWebBuilderCore._SiteData.MyLayouts.Details
                 Date = When,
                 Link = "MyLayouts/index.html",
                 Paragraph = "New Loco and more wagons",
-                KeyImage = $@"MyLayouts\images\{When.ToString("yyyyMMdd")}\IMG_4981-thumb.JPG",
+                KeyImage = $@"MyLayouts\images\{When.ToString("yyyyMMdd")}\IMG_5289-thumb.JPG",
             };
         }
     }
