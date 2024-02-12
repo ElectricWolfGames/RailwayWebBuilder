c84afcbf2618a59c688362f45e43ee21b49a3923
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2024-02-12 16:31:01 +0000

    Update files

--- RailwayWebBuilderCore/_SiteData/MyLayouts/Details/Layout_2024_02_18_Episode_63.cs
+++ RailwayWebBuilderCore/_SiteData/MyLayouts/Details/Layout_2024_02_18_Episode_63.cs
@@ -4,26 +4,26 @@ using System;
 
 namespace RailwayWebBuilderCore._SiteData.MyLayouts.Details
 {
-    public class Layout_2024_01_28_Episode_62 : LayoutBase, IBlogger
+    public class Layout_2024_02_18_Episode_63 : LayoutBase, IBlogger
     {
-        public Layout_2024_01_28_Episode_62()
+        public Layout_2024_02_18_Episode_63()
         {
             Active = true;
-            When = new DateTime(2024, 01, 28);
-            EpisodeNumber = 62;
+            When = new DateTime(2024, 02, 18);
+            EpisodeNumber = 63;
 
             InitDatedPaths();
 
-            Title = "<h2>62: Full Layout tour</h2>";
+            Title = "<h2>63: Time for the Hill to go!</h2>";
             Details.Append("<p>Hello and welcome back to Karl and Debbie trains, And we have another Cattington model railway layout update for you.</p>");
             Details.Append("</br>");
-            Details.Append("<p>Today I would like to show you around the layout</p>");
-            Details.Append("<p>The layout is 9 by 8 feet</p>");
-            Details.Append("<p>And it's now 3 years old.</p>");
+            Details.Append("<p>I'll be removing the hill, this is letting me change some of the layout</p>");
+            Details.Append("<p>this also allows us to add in some extra sidings.</p>");
+            Details.Append("<p>And a big thanks to Steve for the new Cattington Mugs.</p>");
 
             Details.Append("</br>");
             Details.Append("<p>Thank you for watching, Take care</p>");
-            YouTubeLink = "Wi4SWrfwcxQ";
+            YouTubeLink = "";
         }
 
         public IBlog GetBlog()
@@ -33,8 +33,8 @@ namespace RailwayWebBuilderCore._SiteData.MyLayouts.Details
                 Name = $"Cattington Update {EpisodeNumber}",
                 Date = When,
                 Link = "MyLayouts/index.html",
-                Paragraph = "Full layout tour",
-                KeyImage = $@"MyLayouts\images\{When.ToString("yyyyMMdd")}\IMG_5305-thumb.JPG",
+                Paragraph = "Hill no more",
+                KeyImage = $@"MyLayouts\images\{When.ToString("yyyyMMdd")}\IMG_5350-Cattington-thumb.JPG",
             };
         }
     }
