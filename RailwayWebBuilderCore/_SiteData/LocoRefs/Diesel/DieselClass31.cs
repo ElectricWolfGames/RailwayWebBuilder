3155ad6adf8fad9fa3a5b76b0d339a31a194f922
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2024-03-22 07:15:04 +0000

    Added Locos

--- RailwayWebBuilderCore/_SiteData/LocoRefs/Diesel/DieselClass31.cs
+++ RailwayWebBuilderCore/_SiteData/LocoRefs/Diesel/DieselClass31.cs
@@ -4,21 +4,19 @@ namespace RailwayWebBuilderCore._SiteData.LocoRefs.Diesel
 {
     public class DieselClass31 : DieselClassBase
     {
-        // TODO: CLass 31 missing paragraphs
         public DieselClass31()
         {
             ClassName = "Class 31";
             StockType = StockTypes.Diesel;
 
-            Paragraph1 = "";
-            Paragraph2 = "";
+            Paragraph1 = "The British Rail Class 31 diesel locomotives, also known as the Brush Type 2 and previously as Class 30, were built by Brush Traction from 1957 to 1962. They were numbered in two series, D5500-D5699 and D5800-D5862.";
+            Paragraph2 = "Construction of the first locomotive was completed in the final week of September 1957, and the handing-over took place on 31 October. The first Class 31 entered service in November 1957, after the launch of the Class 20 locomotive and was one of the Pilot Scheme locomotives ordered by British Railways to replace steam traction.";
             Paragraph3 = "";
-
-            Keywords.Add("31466");
 
             LocoNumbers.AddRange(new[]
             {
                 new LocoDetails("31466"),
+                new LocoDetails("31210"),
                 });
         }
     }
---
+++ RailwayWebBuilderCore/_SiteData/LocoRefs/Steam/SteamLNERThompsonClassB1.cs
@@ -0,0 +1,21 @@
+﻿using RailwayWebBuilderCore.Enums;
+
+namespace RailwayWebBuilderCore._SiteData.LocoRefs.Steam
+{
+    public class SteamLNERThompsonClassB1 : SteamClassBase
+    {
+        public SteamLNERThompsonClassB1()
+        {
+            ClassName = "LNER Thompson Class B1";
+            StockType = StockTypes.SteamLoco;
+
+            Paragraph1 = "The London and North Eastern Railway (LNER) Thompson Class B1 is a class of steam locomotive designed by Edward Thompson for medium mixed traffic work.";
+            Paragraph2 = "It was the LNER's equivalent to the highly successful GWR Hall Class and the LMS Stanier Black Five, two-cylinder mixed traffic 4-6-0s. However, it had the additional requirement of having to be cheap because, due to wartime and post-war economies, the LNER, never the richest railway company, had to make savings.";
+
+            LocoNumbers.AddRange(new[]
+            {
+                new LocoDetails("1264"),
+                });
+        }
+    }
+}
