f1d2ab2af286295d2e586b6b63189464eec77c3d
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2024-03-28 06:32:22 +0000

    Added Episode 65

--- RailwayWebBuilderCore/_SiteData/LocoRefs/Diesel/DieselClass55.cs
+++ RailwayWebBuilderCore/_SiteData/LocoRefs/Diesel/DieselClass55.cs
@@ -14,7 +14,7 @@ namespace RailwayWebBuilderCore._SiteData.LocoRefs.Diesel
                             "They were designed for the high-speed express passenger services on the East Coast Main Line (ECML) between Edinburgh and London King's Cross. " +
                             "They gained the name 'Deltic' from the prototype locomotive, DP1 Deltic (the running number DP1 was never carried), " +
                             "which in turn was named after its Napier Deltic power units. At the time of their introduction into service, the Class 55s were the most powerful single-unit diesel locomotives in the world, " +
-                            "with a power output of 3,300 hp (2,500 kW).[3] They had an official maximum speed of 100 miles per hour (160 km/h), however this was frequently exceeded, especially in their later years of service, " +
+                            "with a power output of 3,300 hp (2,500 kW). They had an official maximum speed of 100 miles per hour (160 km/h), however this was frequently exceeded, especially in their later years of service, " +
                             "with speeds of up to 117 miles per hour (188 km/h), being recorded on level gradients, and up to 125 miles per hour (201 km/h) whilst decending Stoke Bank";
             Paragraph2 = "";
             Paragraph3 = "";
@@ -29,6 +29,7 @@ namespace RailwayWebBuilderCore._SiteData.LocoRefs.Diesel
 
             LocoNumbers.AddRange(new[] {
                 new LocoDetails("55009"),
+                new LocoDetails("55013"),
                 new LocoDetails("55019"),
                 });
         }
