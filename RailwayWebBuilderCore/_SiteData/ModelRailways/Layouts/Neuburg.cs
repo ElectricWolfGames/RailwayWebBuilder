5444495c44ef4d74b451ccd9567aabc014950708
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2024-04-30 07:38:58 +0100

    Added more layout to the Layout by Layout

--- RailwayWebBuilderCore/_SiteData/ModelRailways/Layouts/Neuburg.cs
+++ RailwayWebBuilderCore/_SiteData/ModelRailways/Layouts/Neuburg.cs
@@ -1,8 +1,9 @@
 ﻿using RailwayWebBuilderCore.Enums;
+using RailwayWebBuilderCore.Interfaces;
 
 namespace RailwayWebBuilderCore._SiteData.ModelRailways.Layouts
 {
-    internal class Neuburg : LayoutBase
+    internal class Neuburg : LayoutBase, ILayoutByLayout
     {
         public Neuburg()
         {
@@ -12,6 +13,8 @@ namespace RailwayWebBuilderCore._SiteData.ModelRailways.Layouts
             Description = "Neuburg portrays a small terminus station set in the former East Germany (DDR) in the 1980s, shortly before reunification. </br></br>" +
                 " There is a small industrial complex that provides various types of freight traffic. Passenger traffic is a mix of local and regional trains. </br></br>" +
                 " Motive power is mainly diesel but there is the occasional steam visitor.";
+
+            LayoutByLayoutVideo = "";
         }
     }
 }
--- RailwayWebBuilderCore/_SiteData/ModelRailways/Layouts/NewDalbyResearchDivision.cs
+++ RailwayWebBuilderCore/_SiteData/ModelRailways/Layouts/NewDalbyResearchDivision.cs
@@ -1,14 +1,16 @@
 ﻿using RailwayWebBuilderCore.Enums;
+using RailwayWebBuilderCore.Interfaces;
 
 namespace RailwayWebBuilderCore._SiteData.ModelRailways.Layouts
 {
-    internal class NewDalbyResearchDivision : LayoutBase
+    internal class NewDalbyResearchDivision : LayoutBase, ILayoutByLayout
     {
         public NewDalbyResearchDivision()
         {
             Name = LayoutNamesEnums.NewDalbyResearchDivision;
             Owner = "Andrew Porter";
             Description = ""; // TODO Needs description
+            LayoutByLayoutVideo = "";
         }
     }
 }
