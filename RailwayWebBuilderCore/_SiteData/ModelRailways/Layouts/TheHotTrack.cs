6b4f80d659e5e2440f8833f523275356c2f69113
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2024-05-12 09:16:49 +0100

    Added more layout details

---
+++ RailwayWebBuilderCore/_SiteData/ModelRailways/Layouts/TheHotTrack.cs
@@ -0,0 +1,14 @@
+﻿using RailwayWebBuilderCore.Enums;
+
+namespace RailwayWebBuilderCore._SiteData.ModelRailways.Layouts
+{
+    internal class TheHotTrack : LayoutBase
+    {
+        public TheHotTrack()
+        {
+            Name = LayoutNamesEnums.TheHotTrack;
+            Owner = "";
+            Description = "The four mile long, two track railway known as 'The Hot Metal Track' opened in 1979 to transport molten iron from the new Redcar Blast Furnace on Teesside directly to the BOC plant at the Lackenby Steelworks to become world renowned Teesside steel. The line was substantially constructed for the traffic was heavy and continuous; each wagon carrying 320 tonnes of molten iron in this around the clock operation. This layout gives a small glimpse of some of the rail operations in a UK steel works. Most obviously there is the constant flow of molten iron from the blast furnaces to the steel plant using the iconic 'Torpedo' wagons. These travel on the Hot Metal Track which crosses the works entrance on a high level girder bridge proudly 'Passionate About Steel'. The yard behind sees internal user wagons and vehicles from the national network being moved around the works. Variety is the spice of life on an industrial railway and the colourful locomotive fleet includes ex-BR Class 20s, the former Norwegian Railways (NSB) Di8 Class imported spe-cially for this line, Yorkshire Engine Company 'Janus' 0-6-0 diesel electric locos, the Scunthorpe Bo-Bo Hunslets and Port Talbot Brush Bagnall locomotives to name but a few. The Appleby Frodingham Railway Preservation Society operate brake van tours at the Scun-thorpe Steelworks.";
+        }
+    }
+}
