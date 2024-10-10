916aa8fbecaafec346a8c39c99954df4fe43b816
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2024-10-10 07:06:40 +0100

    update TanYCoed Description

--- RailwayWebBuilderCore/_SiteData/ModelRailways/Layouts/TanYCoed.cs
+++ RailwayWebBuilderCore/_SiteData/ModelRailways/Layouts/TanYCoed.cs
@@ -8,7 +8,7 @@ namespace RailwayWebBuilderCore._SiteData.ModelRailways.Layouts
         {
             Name = LayoutNamesEnums.TanYCoed;
             Owner = "";
-            Description = "";  // TODO TanYCoed Needs description
+            Description = "Tan-y-coed is basically a station with incoming trains from either direction with a hidden fiddle yard. Recently purchase by Bernard and Peter to expand their exhibition possibilities. The layout is completely WiFi Z21 running off our phone: with sound on each engine, which are mostly Ffestiniog outline. It has a new lease of life with some modifications.";
         }
     }
 }
