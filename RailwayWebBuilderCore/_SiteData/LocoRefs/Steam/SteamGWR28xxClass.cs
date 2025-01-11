d5d79c7d2a78b5f6d5e40ac75a2f2f0fa0da3884
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-01-11 13:56:35 +0000

    Update Locos

---
+++ RailwayWebBuilderCore/_SiteData/LocoRefs/Steam/SteamGWR28xxClass.cs
@@ -0,0 +1,22 @@
+﻿using RailwayWebBuilderCore.Enums;
+
+namespace RailwayWebBuilderCore._SiteData.LocoRefs.Steam
+{
+    public class SteamGWR28xxClass : SteamClassBase
+    {
+        public SteamGWR28xxClass()
+        {
+            ClassName = "GWR 28xx Class Pannier";
+            StockType = StockTypes.SteamLoco;
+
+            Paragraph1 = "";
+            Paragraph2 = "";
+            Paragraph3 = "";
+
+            LocoNumbers.AddRange(new[]
+            {
+                new LocoDetails("2807"),
+                });
+        }
+    }
+}
---
+++ RailwayWebBuilderCore/_SiteData/LocoRefs/Steam/SteamGWRPannierTank.cs
@@ -0,0 +1,22 @@
+﻿using RailwayWebBuilderCore.Enums;
+
+namespace RailwayWebBuilderCore._SiteData.LocoRefs.Steam
+{
+    public class SteamGWRPannierTank : SteamClassBase
+    {
+        public SteamGWRPannierTank()
+        {
+            ClassName = "GWR Pannier";
+            StockType = StockTypes.SteamLoco;
+
+            Paragraph1 = "";
+            Paragraph2 = "";
+            Paragraph3 = "";
+
+            LocoNumbers.AddRange(new[]
+            {
+                new LocoDetails("1369"),
+                });
+        }
+    }
+}
