39aba55b41cbfcd7c5343eddceeb57f54919b20e
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-10-23 07:05:59 +0100

    added decsription

--- RailwayWebBuilderCore/_SiteData/LocoRefs/Steam/WebbCoalTank.cs
+++ RailwayWebBuilderCore/_SiteData/LocoRefs/Steam/WebbCoalTank.cs
@@ -2,8 +2,6 @@
 
 namespace RailwayWebBuilderCore._SiteData.LocoRefs.Steam;
 
-#warning Need to add description WebbCoalTank
-
 public class WebbCoalTank : SteamClassBase
 {
     public WebbCoalTank()
@@ -11,7 +9,8 @@ public class WebbCoalTank : SteamClassBase
         ClassName = "Webb Coal Tank (LNWR)";
         StockType = StockTypes.SteamLoco;
 
-        Paragraph1 = "";
+        Paragraph1 = "The London and North Western Railway (LNWR) Webb Coal Tank is a class of 0-6-2T steam locomotive. They were called 'Coal Tanks' because they were a side tank version of Webb's standard LNWR 17in Coal Engine, an 0-6-0 tender engine for slow freight trains.";
+        Paragraph2 = "The design was introduced in 1881 by F.W. Webb and had the same cheaply produced cast iron wheels and H-section spokes as the tender engines. A trailing radial axle supporting the bunker was added also with two similarly cast iron wheels. Three hundred were built between 1881 and 1897.";
 
         Keywords.Add("Webb Coal Tank");
         Keywords.Add("1054");
