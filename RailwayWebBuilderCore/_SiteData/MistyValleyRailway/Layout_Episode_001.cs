f984e7d05f5eccedc3bf6cd9254904bcddc42725
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-12-21 09:31:52 +0000

    update 001

--- RailwayWebBuilderCore/_SiteData/MistyValleyRailway/Layout_Episode_001.cs
+++ RailwayWebBuilderCore/_SiteData/MistyValleyRailway/Layout_Episode_001.cs
@@ -1,4 +1,5 @@
 ﻿using RailwayWebBuilderCore._SiteData.MyLayouts.Details;
+using RailwayWebBuilderCore.Configuration;
 using RailwayWebBuilderCore.Data;
 using RailwayWebBuilderCore.Interfaces;
 using System;
@@ -10,18 +11,20 @@ public class Layout_Episode_001 : MistyLayoutBase, IBlogger
     public Layout_Episode_001()
     {
         Active = true;
-        When = new DateTime(2026, 01, 01);
+        When = new DateTime(2025, 12, 20);
         EpisodeNumber = 001;
 
         InitDatedPaths();
 
-        Title = "<h2>001: The Misty Valley Railway</h2>";
+        Title = "<h2>001: Name of the Layout | Model Railway Episode 1</h2>";
         Details.Append("<p>Here we announce the name of our new layout, and show you the progress so far.</p>");
         Details.Append("<p></p>");
+        Details.Append("<p>This new layout is going to be called 'Misty Vallety Railway'</p>");
         Details.Append("<p></p>");
+        Details.Append("<p>I've started to replace the tempary wooden supports with cut wood.</p>");
 
         Details.Append("</br>");
-        YouTubeLink = "??????????";
+        YouTubeLink = "https://youtu.be/NjXjlw-5p0s";
     }
 
     public IBlog GetBlog()
@@ -30,7 +33,7 @@ public class Layout_Episode_001 : MistyLayoutBase, IBlogger
         {
             Name = $"The Misty Valley Railway {EpisodeNumber}",
             Date = When,
-            Link = "MVRLayouts/index.html",
+            Link = $"{Constants.MVRLayouts}/index.html",
             Paragraph = "Named",
             KeyImage = $@"MVRLayouts\images\{When.ToString("yyyyMMdd")}\IMG_8488-thumb.JPG",
         };
