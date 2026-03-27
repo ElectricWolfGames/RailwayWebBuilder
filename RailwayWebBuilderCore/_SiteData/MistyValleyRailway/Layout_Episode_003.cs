8960a204557e23d1cd9f16d669ac95a0792fa150
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2026-03-27 06:39:08 +0000

    Addec cotgrave show

---
+++ RailwayWebBuilderCore/_SiteData/MistyValleyRailway/Layout_Episode_003.cs
@@ -0,0 +1,39 @@
+﻿using RailwayWebBuilderCore._SiteData.MyLayouts.Details;
+using RailwayWebBuilderCore.Configuration;
+using RailwayWebBuilderCore.Data;
+using RailwayWebBuilderCore.Interfaces;
+using System;
+
+namespace RailwayWebBuilderCore._SiteData.MistyValleyRailway;
+
+public class Layout_Episode_003 : MistyLayoutBase, IBlogger
+{
+    public Layout_Episode_003()
+    {
+        Active = true;
+        When = new DateTime(2026, 03, 14);
+        EpisodeNumber = 003;
+
+        InitDatedPaths();
+
+        Title = "<h2>003: Embankment and Bridges | Model Railway Episode 3</h2>";
+        Details.Append("<p>Here Misty Valley railway</p>");
+        Details.Append("<p></p>");
+        Details.Append("<p>we have a new bridge to show and some work on the Embankment</p>");
+
+        Details.Append("</br>");
+        YouTubeLink = "tnANGcHqig8";
+    }
+
+    public IBlog GetBlog()
+    {
+        return new Blog
+        {
+            Name = $"The Misty Valley Railway {EpisodeNumber}",
+            Date = When,
+            Link = $"{Constants.MVRLayouts}/index.html",
+            Paragraph = "Embankment and Bridges",
+            KeyImage = $@"{Constants.MVRLayouts}\images\{When.ToString("yyyyMMdd")}\IMG_8901.JPG",
+        };
+    }
+}
---
+++ RailwayWebBuilderCore/_SiteData/ModelRailways/Events/Years2026/01 BinghamModelRailway2026.cs
@@ -0,0 +1,44 @@
+﻿using RailwayWebBuilderCore._SiteData.ModelRailways.EventLocations;
+using RailwayWebBuilderCore.Configuration;
+using RailwayWebBuilderCore.Data;
+using RailwayWebBuilderCore.Enums;
+using RailwayWebBuilderCore.Interfaces;
+using System;
+
+namespace RailwayWebBuilderCore._SiteData.ModelRailways.Events.Years2026;
+
+public class BinghamModelRailway2026 : ModelEventPageDetails, IBlogger
+{
+    public BinghamModelRailway2026()
+    {
+        Name = "Bingham Model Railway Club";
+        Location = new BinghamModelRailwayClub();
+        EventDates = "28th / 29th March";
+        Descrption = "Bingham Model Railway Club's 15th Annual Show";
+        ImagesPath = Constants.RawDataPath + @"ModelEvents\2026-03-28 Bingham Model Railway Club";
+        TripDate = new DateTime(2026, 03, 28);
+        YouTubeLink = "https://www.youtube.com/embed/?????";
+        ImageFolder = "20250328-Cotgrave";
+        Title = "Bingham Model Railway Show 2026";
+        ImagePreview = "IMG_5837-???-thumb.JPG";
+
+        Layouts.Add(new LayoutDetails(LayoutNamesEnums.Ashtown));
+
+        Keywords.AddRange(ModelEventKeywords());
+        Keywords.AddRange(new string[] {
+            "Bingham Model Railway Show 2026"
+            });
+    }
+
+    public IBlog GetBlog()
+    {
+        return new Blog
+        {
+            Name = "Bingham Model Railway Show 2026",
+            Date = new DateTime(2026, 03, 28),
+            Paragraph = "Visited The Bingham Model Railway Club's 16th Annual Show",
+            Link = $"{Constants.ModelEvents}/20260328-Cotgrave/index.html",
+            ModelEvent = this,
+        };
+    }
+}
