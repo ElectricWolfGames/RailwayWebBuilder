b76df34355c7be584a076af6be70c8c5855d8d08
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2026-08-04 07:03:58 +0100

    Added Market Bosworth model show

---
+++ RailwayWebBuilderCore/_SiteData/ModelRailways/EventLocations/MarketBosworthStation.cs
@@ -0,0 +1,13 @@
+﻿using RailwayWebBuilderCore.Data;
+
+namespace RailwayWebBuilderCore._SiteData.ModelRailways.EventLocations;
+
+public class MarketBosworthStation : EventLocationsBase
+{
+    public MarketBosworthStation()
+    {
+        Name = "Lutterworth Sports Centre";
+        Address = "Sedgemere Road, Market Bosworth Station Road\r\nHinckley and Bosworth, Leicestershire, CV13 0AY";
+        PostCode = "CV13 0AY";
+    }
+}
